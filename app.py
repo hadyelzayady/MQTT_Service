c6e6e37c1eefaff0765cd16b5c5d1ce342963ce6
hadye <hadyelzayady@yahoo.com>
2018-06-25 19:02:33 +0200

    basic fn: publish, connect, message arrived,subscribe hardcoded data

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import paho.mqtt.client as mqtt
+app = Flask(__name__)
+
+broker="broker.mqttdashboard.com"
+@app.route('/')
+def main():
+    return render_template("base.html")
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
